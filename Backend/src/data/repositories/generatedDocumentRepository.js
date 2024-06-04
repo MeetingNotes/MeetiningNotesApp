5ea07b8d2b9560102295acaf56da5ca54680c553
Thato-Tladi <thato.tladi@bbd.co.za>
2024-06-04 14:18:12 +0200

    Database cleaned, API initalized with db

---
+++ Backend/src/data/repositories/generatedDocumentRepository.js
@@ -0,0 +1,29 @@
+const { GeneratedDocument, UploadedDocument } = require('../models');
+
+const createGeneratedDocument = async (documentData) => {
+  return await GeneratedDocument.create(documentData);
+};
+
+const findGeneratedDocumentById = async (generated_doc_id) => {
+  return await GeneratedDocument.findByPk(generated_doc_id);
+};
+
+const findGeneratedDocumentByUploadedId = async (uploaded_doc_id) => {
+    return await GeneratedDocument.findOne({ where: { uploaded_doc_id } });
+};
+
+const findAllGeneratedDocumentsByUserId = async (user_id, limit, offset) => {
+    return await GeneratedDocument.findAll({
+        include: [
+            {
+                model: UploadedDocument,
+                where: { user_id },
+                required: true
+            }
+        ],
+        limit,
+        offset
+    });
+};
+
+module.exports = { createGeneratedDocument, findGeneratedDocumentById,  findGeneratedDocumentByUploadedId, findAllGeneratedDocumentsByUserId };
