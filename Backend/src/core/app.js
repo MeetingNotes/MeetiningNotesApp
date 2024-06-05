5d9a057bced39c072a164bbde03e04d729e88844
Thato-Tladi <thato.tladi@bbd.co.za>
2024-06-05 22:06:36 +0200

    CORS Added

--- Backend/src/core/app.js
+++ Backend/src/core/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const corsMiddleware = require('./middlewares/corsMiddleware');
 
 dotenv.config();
 
 const app = express();
+
+app.use(corsMiddleware);
 
 app.use(bodyParser.json());
 
---
+++ Backend/src/core/middlewares/corsMiddleware.js
@@ -0,0 +1,22 @@
+const cors = require('cors');
+
+const allowedOrigins = [
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+
+const corsMiddleware = cors(corsOptions);
+
+module.exports = corsMiddleware;
